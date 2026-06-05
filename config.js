7539de0f9049c20d006d428eecd2b87a08bda896
fmabdullah0902772-dev <fmabdullah0902772@gmail.com>
2026-06-05 23:42:21 +0500

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
     // 1. CONFIGURATION DE BASE (Session & Database)
     // ===========================================================
     SESSION_ID: process.env.SESSION_ID || "MINI BOT", 
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://fmabdullah10_db_user:LDOuPN828qjv9BT4@cluster0.cqxupry.mongodb.net/?retryWrites=true&w=majority',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://abdullahmalik634958_db_user:sBamZe0GI8OLTYCw@cluster0.92ew1v7.mongodb.net/?appName=Cluster0',
     
     // ===========================================================
     // 2. INFORMATIONS DU BOT
