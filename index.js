82c3f06d114a6aebf9382a2882e6d36ebbba5fd4
fmabdullah0902772-dev <fmabdullah0902772@gmail.com>
2026-06-06 11:59:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const pairRouter = require('./command');
+const pairRouter = require('./inconnu');
 app.use('/', pairRouter);
 
 app.listen(port, () => {
